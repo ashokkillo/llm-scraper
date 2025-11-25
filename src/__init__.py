4f9dd598c80ddb622e7e40c6a668ebd999316aa4
ashokkillo <72614707+ashokkillo@users.noreply.github.com>
2025-11-25 08:46:49 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,11 @@
+"""
+Web Scraping Tutor
+Provides scraping, transformation, and checkpoint management utilities.
+"""
+
+from .scraper import run_scraper
+from .jira_client import JiraClient
+from .transform import transform_issue
+from .state import StateManager
+from .utils import setup_logging
 
